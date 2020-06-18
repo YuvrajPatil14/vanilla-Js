2a0eb05f1d4dedaf0486cc2e7382ba92e03fcde7
YuvrajPatil14 <yuvrajpatil1441998@gmail.com>
2020-06-18 19:24:57 +0530

    one with the exchange rate

---
+++ -o2-d3js/app.js
@@ -0,0 +1,44 @@
+const DUMMY_DATA = [
+  {
+    id: "d1",
+    value: 10,
+    region: "USA",
+  },
+  {
+    id: "d2",
+    value: 14,
+    region: "USB",
+  },
+  {
+    id: "d3",
+    value: 16,
+    region: "USC",
+  },
+  {
+    id: "d4",
+    value: 17,
+    region: "USD",
+  },
+];
+
+const xScale = d3
+  .scaleBand()
+  .domain(DUMMY_DATA.map((datapoint) => datapoint.region))
+  .rangeRound([0, 250])
+  .padding(0.1);
+const yScale = d3.scaleLinear().domain([0, 20]).range([200, 0]);
+const container = d3.select("svg").classed("container", true);
+const bars = container
+  .selectAll(".bar")
+  .data(DUMMY_DATA)
+  .enter()
+  .append("rect")
+  .classed("bar", true)
+  .attr("width", xScale.bandwidth())
+  .attr("height", (data) => 200 - yScale(data.value))
+  .attr("x", (data) => xScale(data.region))
+  .attr("y", (data) => yScale(data.value));
+
+setTimeout(() => {
+  bars.data(DUMMY_DATA.slice(0, 2)).exit();
+}, 2000);
---
+++ 04-exchange-rate/script.js
@@ -0,0 +1,36 @@
+const currencyElem_one = document.getElementById("currency-one");
+const currencyElem_two = document.getElementById("currency-two");
+const amountElem_two = document.getElementById("amount-two");
+const amountElem_one = document.getElementById("amount-one");
+const rateElem = document.getElementById("rate");
+const swap = document.getElementById("swap");
+
+function calculate() {
+  const currency_one = currencyElem_one.value;
+  const currency_two = currencyElem_two.value;
+
+  fetch(`https://api.exchangerate-api.com/v4/latest/${currency_one}`)
+    .then((res) => res.json())
+    .then((data) => {
+      // console.log(data.rates);
+
+      const rate = data.rates[currency_two];
+      //console.log(rate);
+      rateElem.innerText = ` 1 ${currency_one} = ${rate} ${currency_two}`;
+      amountElem_two.value = (amountElem_one.value * rate).toFixed(2);
+    });
+}
+
+//event listeners
+
+currencyElem_one.addEventListener("change", calculate);
+amountElem_one.addEventListener("input", calculate);
+currencyElem_two.addEventListener("change", calculate);
+amountElem_two.addEventListener("input", calculate);
+swap.addEventListener("click", () => {
+  const temp = currencyElem_one.value;
+  currencyElem_one.value = currencyElem_two.value;
+  currencyElem_two.value = temp;
+  calculate();
+});
+calculate();
